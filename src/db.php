1a693694b0da4eb96f9515f8ee999193de6413d7
etolu <148735218+etolu@users.noreply.github.com>
2024-01-10 14:31:38 +0900

    Create db.php

---
+++ src/db.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql215.phy.lolipop.lan';
+    const DBNAME = 'LAA1518101-final';
+    const USER = 'LAA1518101';
+    const PASS = 'Pass1217';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
